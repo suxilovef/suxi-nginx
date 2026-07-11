139decf21240216cccf6a1034a5440d94364d38b
suxilovef <suxilovef@gmail.com>
2026-07-11 16:04:20 +0800

    Initial commit: Spring Boot Nginx application

---
+++ src/test/java/com/sw/yang/nginx/NginxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.yang.nginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
